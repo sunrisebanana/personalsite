f2b6062c9c8c8a4a81235f8567a24fe56d248d53
jaime herzog <sunrisebanana@gmail.com>
2021-03-10 21:59:41 -0500

    fixed redundancy issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 import 'semantic-ui-css/semantic.min.css'
-import { LoremIpsum } from 'react-lorem-ipsum';
 
 const IndexPage = () => (
   <Layout title="Home">
