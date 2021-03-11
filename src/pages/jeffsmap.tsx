f03bcfe4fa39f3b9246ae9169c08f38307543b6b
jaime herzog <sunrisebanana@gmail.com>
2021-03-11 15:46:34 -0500

    changed size of map

--- src/pages/jeffsmap.tsx
+++ src/pages/jeffsmap.tsx
@@ -5,7 +5,7 @@ import 'semantic-ui-css/semantic.min.css'
 const JeffsMapPage = () => (
 
   <Layout title="Home">
-      <iframe src="https://srv2.zoomable.ca/viewer.php?i=ZoomableAlgonquinMapRESIZEDv10" width={600} height={450} frameBorder={0} style={{border: 0}} allowFullScreen />
+      <iframe src="https://srv2.zoomable.ca/viewer.php?i=ZoomableAlgonquinMapRESIZEDv10" width='100%' height='900' frameBorder={0} style={{border: 0}} allowFullScreen />
   </Layout>
 )
 
