a310157e5c93d3d17ed7f04071c324bdc0daa153
jaime herzog <sunrisebanana@gmail.com>
2021-03-11 16:02:13 -0500

    updated map

--- src/pages/jeffsmap.tsx
+++ src/pages/jeffsmap.tsx
@@ -7,7 +7,7 @@ const JeffsMapPage = () => (
 
   <Layout title="Home">
       <Header size='huge'>Jeff's Map of Algonquin Park</Header>
-      <p>Thank you to Jeff for your fantastic map, and your creative commons licensing. </p>
+      <p>Thank you to Jeff for your fantastic map, and the creative commons licensing on the map. </p>
       <iframe src="https://srv2.zoomable.ca/viewer.php?i=ZoomableAlgonquinMapRESIZEDv10" width='100%' height='800' frameBorder={0} style={{border: 0}} allowFullScreen />
   </Layout>
 )
