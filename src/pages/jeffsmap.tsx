8f8b4ce869cc9db26dd90fb22b8634b6973dba5f
jaime herzog <sunrisebanana@gmail.com>
2021-03-11 16:01:49 -0500

    updated jeff

--- src/pages/jeffsmap.tsx
+++ src/pages/jeffsmap.tsx
@@ -1,11 +1,14 @@
 
 import Layout from '../components/Layout'
 import 'semantic-ui-css/semantic.min.css'
+import { Header } from 'semantic-ui-react'
 
 const JeffsMapPage = () => (
 
   <Layout title="Home">
-      <iframe src="https://srv2.zoomable.ca/viewer.php?i=ZoomableAlgonquinMapRESIZEDv10" width='100%' height='900' frameBorder={0} style={{border: 0}} allowFullScreen />
+      <Header size='huge'>Jeff's Map of Algonquin Park</Header>
+      <p>Thank you to Jeff for your fantastic map, and your creative commons licensing. </p>
+      <iframe src="https://srv2.zoomable.ca/viewer.php?i=ZoomableAlgonquinMapRESIZEDv10" width='100%' height='800' frameBorder={0} style={{border: 0}} allowFullScreen />
   </Layout>
 )
 
