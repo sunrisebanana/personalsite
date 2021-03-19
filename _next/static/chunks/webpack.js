167bb5a30a40301ac7b41c89102a29e2161cc82d
jaime herzog <sunrisebanana@gmail.com>
2021-03-19 13:10:54 -0400

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0bbe56b1dae472b1238a";
+/******/ 	var hotCurrentHash = "a011520320c0d14618cf";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
