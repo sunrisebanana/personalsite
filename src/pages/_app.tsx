550ca513b8daa92ecbc1b8074f3eae02f931b74c
sunrisebanana <sunrisebanana@gmail.com>
2025-11-20 23:28:24 -0500

    - Removed broken touch command from predeploy - Added explicit semantic css import which when not included was breaking the menu widget

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 // import App from "next/app";
 import type { AppProps /*, AppContext */ } from 'next/app'
 import '../styles/global.css'
+import 'semantic-ui-css/semantic.min.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
